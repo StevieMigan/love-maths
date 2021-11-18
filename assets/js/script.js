a6b4c3c9cd67b64e4b8e1b1491f4cb567421190f
Stevie Migan <stevie@radcub.net>
2021-11-18 15:55:40 +0000

    Add multiply game mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,41 @@
 //Wait for the DOM to finish loading before running the game
 //Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
-  let buttons = document.getElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
 
-  for (let button of buttons){
-      button.addEventListener("click", function() {
-          if (this.getAttribute("data-type") === "submit") {
-              checkAnswer();
-          } else {
-            let gameType = this.getAttribute("data-type");
-            runGame(gameType);
-          }
-      })
-  }
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        })
+    }
 
-  runGame("addition");
+    runGame("addition");
 })
 
 /** 
-* The main game "loop", called when the Script is first loaded
-* and after the user's answer has been processed
-*/
+ * The main game "loop", called when the Script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame(gameType) {
 
     // Creates two random numbers between 1 and 25
-  let num1 = Math.floor(Math.random() * 25) + 1;
-  let num2 = Math.floor(Math.random() * 25) + 1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-  if (gameType === "addition") {
-      displayAdditionQuestion(num1, num2);
-  }else {
-          alert(`Unknown game type: ${gameType}`)
-          throw `Unknown game type: ${gameType}. Aborting!`;
-  }
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`)
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 /**
@@ -41,20 +43,20 @@ function runGame(gameType) {
  * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
-  
-let userAnswer = parseInt(document.getElementById("answer-box").value);
-let calculatedAnswer = calculateCorrectAnswer();
-let isCorrect = userAnswer === calculatedAnswer[0];
 
-if (isCorrect) {
-    alert("Hey! You got it right! :D");
-    incrementScore();
-} else {
-    alert(`awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
-    incrementWrongAnswer();
-}
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
-runGame(calculatedAnswer[1]);
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+        incrementScore();
+    } else {
+        alert(`awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        incrementWrongAnswer();
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
 
@@ -69,11 +71,13 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
-    
+
 }
 
 
@@ -81,7 +85,7 @@ function calculateCorrectAnswer() {
  * Gets the current score from the DOM and increments it by 1
  */
 function incrementScore() {
-   
+
     let oldScore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++oldScore;
 }
@@ -104,6 +108,8 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
